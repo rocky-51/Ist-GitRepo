0321dc8e26a23744c63d337c38d99a883bb00ae2
Rocky Bhai <rockybhai909@gmail.com>
2024-12-20 23:18:09 +0530

    STD CALC

--- Calculator_Project.py
+++ Calculator_Project.py
@@ -14,6 +14,13 @@ def sum():
         s+=i
     return s
 
+#   function for subtraction
+def diff():
+    n1=input()
+    n2=input()
+    newn1,newn2=great_num(n1,n2)
+    return int(newn1)-int(newn2)
+    
 #   function for multiplication
 def multi():
     n=int(input("Enter the number of terms:"))
@@ -23,11 +30,24 @@ def multi():
         multi*=i
     return multi
 
+#   function for division
+def div():
+    try:        #try block for handling ZeroDivisionError
+        n1=int(input())
+        n2=int(input())
+        equalto=input("Press = ")
+        if(equalto=='='):
+            print("Quotient= ",n1/n2,"\nRemainder= ",n1%n2)
+    except ZeroDivisionError:
+        print("Doesn't Divide by Zero!!")
+        
 #   output display
 print("CALCULATOR")
-print("----------")
+print("-------------------")
+print("h    std calc")
 print("AC   <-  +/-    /")     
 print("7    8   9      x\n4    5   6      -\n1    2   3      +\n%    0   .      =")
+print("-------------------")
 y=input("Press Y or y to open the calculator: ")
 if(y=='Y' or y=='y'):
     op=input("\nPress the key operation on keyboard:")
@@ -37,25 +57,25 @@ if(y=='Y' or y=='y'):
         if(equalto=='='):
             print("\nSum= ",s)
     elif(op=='-'):  #condition for subtracting
-        n1=input()
-        n2=input()
-        newn1,newn2=great_num(n1,n2)
+        d=diff()
         equalto=input("Press = ")
         if(equalto=='='):
-            print("\nDifference:",(int(newn1)-int(newn2)))
+            print("\nDifference:",d)
     elif(op=='x'):  #condition for multiplication
         m=multi()
         equalto=input("Press = ")
         if(equalto=='='):
             print("\nProduct= ",m)
     elif(op=='/'):  #condition for division
-        try:        #try block for handling ZeroDivisionError
-            n1=int(input())
-            n2=int(input())
-            equalto=input("Press = ")
-            if(equalto=='='):
-                print("Quotient= ",n1/n2,"\nRemainder= ",n1%n2)
-        except ZeroDivisionError:
-            print("Doesn't Divide by Zero!!")
-
+        div()
+    elif(op=='AC'):
+        pass
+    elif(op=='std calc'):
+        print("-----------------------------------------------------")
+        print("1/x    x^2        x^3        y^x   AC   <-  +/-    /")     
+        print("x!     x^(1/2)    y^(1/x)    lg    7    8   9      x\nsin    cos        tan        ln    4    5   6      -")
+        print("cosec  sec        cot        e^x   1    2   3      +")
+        print("\t\t\t\t   %    0   .      =")
+        print("-----------------------------------------------------")
+        
                 
