adbacc5bb36e2feabb147e110c2c1509aa2fc767
Rocky Bhai <pandeyshobhit229@gmail.com>
2025-08-25 18:29:09 +0530

    commit all

--- LoginView.py
+++ LoginView.py
@@ -1,38 +1,65 @@
+from kivy.config import Config
+Config.set('graphics', 'width', '700')  # Set width to 800 pixels
+Config.set('graphics', 'height', '350')
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel,MDIcon
+# from kivymd.uix.label import MDLabel,MDIcon
 from kivymd.uix.screen import Screen
-from kivymd.uix.button import MDFlatButton,MDRectangleFlatButton,MDIconButton,MDFloatingActionButton
-from kivymd.uix.textfield import MDTextField
+# from kivymd.uix.button import MDFlatButton,MDRectangleFlatButton,MDIconButton,MDFloatingActionButton
+# from kivymd.uix.textfield import MDTextField
 from kivy.lang import Builder
 
+expensetracker="""
+MDLabel:
+    text:"Expense Tracker"
+    font_style:"H3"
+    pos_hint:{'top':1.3}
+    halign:'center'
+    valign:'top'
+    theme_text_color:"Hint"
+"""
+
+u_textField="""
+MDTextField:
+    hint_text:"Enter Email "
+    size_hint_x: None
+    width: "200dp"
+    valign:'top'
+    pos_hint: {'bottom':0.5,"center_x": .5, "center_y": .5}
+    mode:'rectangle'
+"""
+p_textField="""
+MDTextField:
+    hint_text:"Enter Password "
+    size_hint_x: None
+    width: "200dp"
+    pos_hint: {"center_x": .5, "center_y": .2,'top':0.35,'bottom':0.5}
+    mode:'rectangle'
+"""
+l_button="""
+MDFlatButton:
+    text:"Login "
+    pos_hint:{ "center_x":.45,"center_y": 0.37,'bottom':0.5,'right':0.8}
+"""
+signupLabel="""
+MDLabel:
+    text:"Don't have an account? Click Here "
+    font_style:"Subtitle2"
+    pos_hint:{"center_x": .85,'top':0.6}
+    theme_text_color:"Primary"
+"""
+
+
 class LoginView(MDApp):
-    def build(self):
+    def show(self):
         screen = Screen()
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "BlueGray"
-        return Builder.load_file('LoginView.py')
-        expenseTrackerLabel = MDLabel(
-            text="Expense Tracker",
-            halign="center",
-            font_style="H1"
-        )
-        usernamefield = MDTextField(
-            hint_text="Enter your username",
-            helper_text="This field is required",
-            helper_text_mode="on_focus",
-            mode="rectangle",
-            pos_hint={'center_x': 0.5, 'center_y': 0.5},
-            size_hint_x=0.8
-        )
-        passwordfield=MDTextField(hint_text="Enter Your Password",password=True,password_mask='.')
-        btn_flt=MDRectangleFlatButton(text='Hello World',pos_hint={'center_x':0.5,'center_y':0.5})
-        signupLabel = MDLabel(
-            text="Don't have an account? click here",
-            halign="center",
-            font_style="H5"
-        )
-        screen.add_widget(text_field)
+        E_Tracker=Builder.load_string(expensetracker)
+        u_text_field=Builder.load_string(u_textField)
+        p_text_field=Builder.load_string(p_textField)
+        loginButton=Builder.load_string(l_button)
+        s_Label=Builder.load_string(signupLabel)
+        s_Label=Builder.load_string(signupLabel)
+        screen.add_widget(E_Tracker,u_text_field,p_text_field)
+        screen.add_widget(loginButton,s_Label)
         return screen
-
-if __name__ == '__main__':
-    LoginView().run()
--- expense-tracker-server.py
+++ expense-tracker-server.py
@@ -2,19 +2,10 @@ from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel,MDIcon
 from kivymd.uix.screen import Screen
 from kivymd.uix.button import MDFlatButton,MDRectangleFlatButton,MDIconButton,MDFloatingActionButton
-
+from LoginView import LoginView
 class SimpleApp(MDApp):
-    
     def build(self):
-        screen = Screen()
-        label = MDLabel(
-            text="Welcome to KivyMD!",
-            halign="center",
-            font_style="H5"
-        )
-        icon_Label=MDIcon(text='library-video',halign='center')
-        btn_flt=MDRectangleFlatButton(text='Hello World',pos_hint={'center_x':0.5,'center_y':0.5})
-        icon_btn=MDFloatingActionButton(icon='language-python',pos_hint={'center_x':0.5,'center_y':0.5})
-        screen.add_widget(icon_btn)
-        return screen
+        login_view=LoginView()
+        mainScreen= login_view.show()
+        return mainScreen
 SimpleApp().run()
